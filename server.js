48dbe898b1179b82730ded56023553da47c8d7d8
dchyii <hongyichong@gmail.com>
2025-03-22 11:32:33 +0800

    created backend skeleton

---
+++ server.js
@@ -0,0 +1,37 @@
+require("dotenv").config();
+const express = require("express");
+const session = require("express-session");
+const methodOverride = require("method-override");
+const path = require("path");
+const pool = require("./db");
+const port = process.env.PORT;
+
+const userController = require("./controllers/userController");
+
+const app = express();
+
+//*middelware
+app.use(express.json());
+app.use(methodOverride("_method"));
+app.use(
+  session({
+    secret: process.env.SESSION_SECRET,
+    saveUninitialized: false,
+    resave: false,
+    cookie: {
+      maxAge: 3600000,
+    },
+  })
+);
+
+//*middelware for routes
+app.use("/api/users", userController);
+
+//*routes
+app.get("/", (req, res) => {
+  //   console.log(req.session);
+  //   console.log(req.session.id);
+  res.sendStatus(200);
+});
+
+app.listen(port, () => console.log(`server has started on port ${port}!`));
